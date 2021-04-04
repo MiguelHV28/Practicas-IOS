f5e0803e8012bd160389beb4ef0174bf6107b9f1
CSI Mike <miguel.angel.herven@gmail.com>
2021-04-04 00:41:37 -0600

    Se agrega practica 1: Tipos de Datos

---
+++ Practica1-TiposDeDatos.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+
+var mivariable = 1
+
+var str = "Hola Terricolas" // Variable string
+
+let PI = 3.1416 // Constante Pi
+
+var MiVariable = 2
+
+var miVariable = 3 // Esta manera de escribir se llama CamelCase
+
+print(mivariable)
+print(MiVariable)
+print(miVariable)
+
+var cadena = "Hola Mac IOS"
+
+cadena = "Swift"
+
+let nombre1:String = "Bruce"
+var apellido1:String = "Wayne"
+let saludo1:String = "Hello"
+
+print("Hello \(nombre1) \(apellido1)")
+
+let nombre2:String = "Dick Robin"
+let saludo2:String = "Hello \(nombre2)"
+print(saludo2)
+
+let CORREO = "nosoybatman@justiceleague.com"
+
+
+var entero = 1
+
+var a:Int = 15
+var b:Int = 25
+
+var suma:Int = a + b
+
+var resta:Int = a  - b
+var mult:Int = a * b
+
+
+var x:Double = 10
+var y:Double = 10
+var pi:Double = x/y
+
+var radio:Double = 1.5
+
+var area:Double = pi * ( radio * radio)
+
+var residuo = 30 % 4
+
+var negativo = -40
+var positivo = +40
+
+positivo += 10
+
+var flotante = 1.5
+print(flotante)
+print("El valor del area es: \(area)")
+
+var i:Int = 0 ; var j:Int = 0
+
+//Boolean
+
+var valorpositivo:Bool = true
+
+var valornegativo:Bool = false
+
+var p = 10
+var q = 10
+
+var comparacion:Bool = p == q
+print(comparacion)
+
