fd94748fdcb41c531f6fd3980c9992f08d460ca8
CSI Mike <miguel.angel.herven@gmail.com>
2021-04-04 00:58:19 -0600

    Se agrega practica2: Cadenas (Emojis)

---
+++ Practica2-Cadenas.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let 😜 = "Sonrisa 🧀"
+print(😜)
+
+var strUnicode = "\u{1F496}"
+
+print(strUnicode)
+
+let 💖 = "Corazón"
+ 
+let comida = "🍔"
+let bebida = "🍺"
+let postre = "🍩"
+
+var comanda = comida + bebida + postre
+print("Mesa 1: " + comanda)
+
